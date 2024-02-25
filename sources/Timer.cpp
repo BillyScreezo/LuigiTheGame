ddbf924a8b7d9b8418db1860905d43c268c8984b
BillyScreezo <64576974+BillyScreezo@users.noreply.github.com>
2024-02-25 10:33:30 +0300

    update timer

--- sources/Timer.cpp
+++ sources/Timer.cpp
@@ -1,19 +1,16 @@
 #include "Timer.h"
+#include <ctime>
 
-Timer::Timer() { time0 = 0; time1 = 0; delay = 0; secCounter = 0; }
-void Timer::erase() { time0 = 0; time1 = 0; delay = 0; secCounter = 0; }
-void Timer::set()
-{
-	time0 = clock();
-}
+void Timer::set() { time_start = clock(); }
+
 void Timer::check()
 {
-	time1 = clock();
-	delay = time1 - time0;
+	time_end = clock();
+	delay = time_end - time_start;
+
 	if (delay >= 1000)
 	{
 		secCounter += delay / 1000;
-		time0 = clock();
+		time_start = clock();
 	}
 }
-unsigned int Timer::get() { return secCounter; }
--- sources/headers/Timer.h
+++ sources/headers/Timer.h
@@ -1,14 +1,16 @@
 #pragma once
-#include <ctime>
-class Timer
+
+class Timer final
 {
 private:
-	unsigned int time0, time1, delay;
-	int secCounter;
+	unsigned time_start;
+	unsigned time_end;
+	unsigned delay;
+	unsigned secCounter;
 public:
-	Timer();
-	void erase();
+	Timer() : time_start{ 0 }, time_end{ 0 }, delay{ 0 }, secCounter{ 0 } {};
+	inline void erase() {time_start = 0; time_end = 0; delay = 0; secCounter = 0;}
 	void set();
 	void check();
-	unsigned int get();
+	inline unsigned get() { return secCounter; }
 };
